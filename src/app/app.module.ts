0db8175dc10a5025dc3f5f9c81d0a210769cb3fe
João Carvalho <cjoao7610@gmail.com>
2024-09-18 09:06:52 -0300

    fix: remove safepipe module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { ArduinoComponentsPageModule } from './pages/arduino-components/arduino-components.module';
 import { OrientationWarningComponent } from './components/orientation-warning/orientation-warning.component';
 import { IndexedDBservice } from './core/services/indexed-db.service';
-import { SafePipe } from './shared/pipes/safe.pipe';
+
 @NgModule({
-  declarations: [AppComponent, OrientationWarningComponent, SafePipe],
+  declarations: [AppComponent, OrientationWarningComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, IndexedDBservice],
   bootstrap: [AppComponent],
